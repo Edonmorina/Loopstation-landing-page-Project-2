d9924a08b335ef55a2616c5f57d3235731cbf415
Edon Morina <74024519+Edonmorina@users.noreply.github.com>
2021-03-16 01:29:21 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
+
 const toggle = document.querySelector('.toggle');
-
 const container2 = document.querySelector('.container2');
-
 const links = document.querySelectorAll('.container2 li');
-
 const cardContainer = document.querySelector('.card-container-m');
-
-
+const body = document.querySelector('body');
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
 
 toggle.addEventListener('click' , () => {
+    
     container2.classList.toggle("open");
     cardContainer.classList.toggle('close');
+    body.classList.toggle('close');
     links.forEach(link =>{
         link.classList.toggle('fade');
     })
+
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true ;
+    }else{
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+  
 })
+
+
